e0476c59d99d077f00986b74e4ce5b42992c3afe
free-flow-code <wildmuskaria8@gmail.com>
2023-08-09 17:52:14 +0300

    Added automatic return to the pending orders page.

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.shortcuts import reverse
+from django.shortcuts import reverse, redirect
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.http import HttpResponseRedirect
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.utils.encoding import iri_to_uri
 
 from .models import Product
 from .models import ProductCategory
@@ -32,6 +35,17 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderProductsItemInline
     ]
+
+    def response_post_save_change(self, request, obj):
+        response = super(OrderAdmin, self).response_post_save_change(request, obj)
+        if "next" in request.GET:
+            if url_has_allowed_host_and_scheme(request.GET['next'], None):
+                url = iri_to_uri(request.GET['next'])
+            else:
+                raise
+            return HttpResponseRedirect(url)
+        else:
+            return response
 
 
 @admin.register(Restaurant)
