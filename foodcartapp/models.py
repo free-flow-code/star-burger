37eec2dd9e801334898be3476629d6398b07a9b4
free-flow-code <wildmuskaria8@gmail.com>
2023-08-28 22:17:27 +0300

    Removed unnecessary imports.

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,6 +1,4 @@
-import requests.exceptions
 from django.db import models
-from django.conf import settings
 from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
 from phonenumber_field.modelfields import PhoneNumberField
