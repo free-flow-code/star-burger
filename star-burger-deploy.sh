65189089a9c80e768026b1642fbf68cc219d43bb
free-flow-code <wildmuskaria8@gmail.com>
2023-10-10 16:22:17 +0300

    Update depkoy script.

--- star-burger-deploy.sh
+++ star-burger-deploy.sh
@@ -7,12 +7,11 @@ eval "$(pyenv init -)"
 pyenv activate venv
 pip3 install -r requirements.txt
 echo -e "\e[1;32m Requirements updated.\e[0m"
-echo $(npm install --dev)
-echo $(.node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./")
-echo $(python3 manage.py collectstatic --noinput)
+npm install --dev
+.node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+python3 manage.py collectstatic --noinput
 echo -e "\e[1;32m Frontend updated.\e[0m"
 python3 manage.py migrate --noinput
-pyenv deactivate
 echo -e "\e[1;32m Migrations applied.\e[0m"
 systemctl daemon-reload
 echo -e "\e[1;32m Systemd reloaded.\e[0m"
@@ -20,4 +19,7 @@ systemctl restart star-burger.service
 echo -e "\e[1;32m Starburger restarted.\e[0m"
 systemctl reload nginx.service
 echo -e "\e[1;32m Nginx reloaded.\e[0m"
+curl -X POST https://api.rollbar.com/api/1/deploy \
+        -H "X-Rollbar-Access-Token: $ROLLBAR_TOKEN" \
+        -d "environment=$ROLLBAR_ENV&revision=$(git rev-parse HEAD)"
 echo -e "\e[1;32m Success!\e[0m"
