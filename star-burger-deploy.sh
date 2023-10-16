e2bbc76eb57f41c6f850514fc08de16241e708aa
free-flow-code <wildmuskaria8@gmail.com>
2023-10-16 18:34:04 +0300

    Update star-burger-deploy.sh

--- star-burger-deploy.sh
+++ star-burger-deploy.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 set -e
 cd /opt/star-burger
-git stash
+git fetch
 git pull star-burger master
 echo -e "\e[1;32m Files updated.\e[0m"
 eval "$(pyenv init -)"
 pyenv activate venv
 pip3 install -r requirements.txt
 echo -e "\e[1;32m Requirements updated.\e[0m"
-.node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+npm ci --also=dev
+node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 python3 manage.py collectstatic --noinput
-echo -e "\e[1;32m Frontend collected.\e[0m"
+echo -e "\e[1;32m Frontend updated.\e[0m"
 python3 manage.py migrate --noinput
 echo -e "\e[1;32m Migrations applied.\e[0m"
 systemctl restart star-burger.service
